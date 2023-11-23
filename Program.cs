61d056c22d8ed7026e275e0961334a4996229892
Haytham <haytham818@outlook.com>
2023-11-23 18:17:07 +0800

    test

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ internal static class Program
 {
     static void Main(string[] args)
     {
-        
+        var strs = new string[] { "repadafda", "replace", "refoidpasf" };
+        var result = LeetCodeExam.LongestCommonPrefix.LongestCommonPrefixS1(strs);
+        Console.WriteLine(result);
     }
 }
